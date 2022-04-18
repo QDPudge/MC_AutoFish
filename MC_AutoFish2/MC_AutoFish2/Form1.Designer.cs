ba93bf86fef7c017154d48c3b3a72d02a186f92d
haozhi.wang <haozhi.wang@improve-quality.com>
2022-04-18 16:53:03 +0800

    first commit

---
+++ MC_AutoFish2/MC_AutoFish2/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace MC_AutoFish2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(33, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(156, 16);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "模拟人类（非精确操作）";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 307);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "自动钓鱼";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 165);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(197, 40);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(27, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 36);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "点击开始后鼠标就会自动点击，切换至我的世界窗口，请瞄准音乐盒~";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 21);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "1";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "鼠标点击间隔             毫秒";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(33, 106);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(72, 16);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "封号报警";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(33, 74);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(120, 16);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "每5分钟休息1分钟";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 335);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "钓鱼台自动钓鱼工具";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ MC_AutoFish2/MC_AutoFish2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MC_AutoFish2
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern int mouse_event(int mouseevent, int dx, int dy, int cButtons, int dwExtraInfo);
+        [DllImport("User32")]
+        public extern static void SetCursorPos(int x, int y);
+
+        private bool gofishing=false; 
+        //移动鼠标 
+        const int MOUSEEVENTF_MOVE = 0x0001;
+        //模拟鼠标左键按下 
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //模拟鼠标左键抬起 
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+        //模拟鼠标右键按下 
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        //模拟鼠标右键抬起 
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        //模拟鼠标中键按下 
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        //模拟鼠标中键抬起 
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        //标示是否采用绝对坐标 
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+
+        public Form1()
+        {
+            InitializeComponent();  
+            timer1.Interval = 1000;  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var ps = Process.GetProcessesByName("minecraft").ToList();
+            if (ps == null || ps.Count == 0) { MessageBox.Show("没有检测到游戏，请开启游戏后重试"); return; }
+            gofishing = !gofishing;
+            if (gofishing) {
+                button1.Text = "钓鱼中....";
+                richTextBox1.Add("钓鱼已开启，" + DateTime.Now.ToString("MM-dd HH:mm:ss"));
+                timer1.Enabled = true;
+                timer2.Enabled = checkBox2.Checked;
+                timer3.Enabled = checkBox3.Checked;
+            } 
+            else 
+            { 
+                button1.Text = "开始";
+                richTextBox1.Add("钓鱼已结束，" + DateTime.Now.ToString("MM-dd HH:mm:ss"));
+            };
+            groupBox1.Enabled = !gofishing;
+           
+
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString()==" "&& gofishing)
+            {
+                gofishing = !gofishing; ; //满足条件后执行事件
+                groupBox1.Enabled = !gofishing;
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //连续两次点击--
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0); 
+            if (checkBox1.Checked)
+            {
+                Random RA = new Random();
+                timer1.Interval = RA.Next(Convert.ToInt32(textBox1.Text), (Convert.ToInt32(textBox1.Text) + 500));
+            }
+            Console.WriteLine(timer1.Interval.ToString());
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (gofishing)
+            {
+                timer1.Enabled=false;
+                timer2.Interval = 60000;
+                gofishing = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+                timer2.Interval = 300000;
+                gofishing = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+            if (textBox1.Text.Length==1&& (e.KeyChar==(char)Keys.Delete|| e.KeyChar == (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            var ps = Process.GetProcessesByName("minecraft").ToList();
+            if (ps == null || ps.Count == 0) { MessageBox.Show("检测到游戏已关闭，请查看账号状态！");
+
+                richTextBox1.Add("封号警告：检测到游戏已关闭，请查看账号状态！，" + DateTime.Now.ToString("MM-dd HH:mm:ss"));
+                 }
+        }
+    }
+    public static class TextBoxHelper 
+    {
+        public static void Add(this RichTextBox rtbox, string text)
+        {
+            if (string.IsNullOrEmpty(rtbox.Text))
+            {
+                rtbox.Text = text;
+            }
+            else
+            {
+                rtbox.AppendText(Environment.NewLine);
+                rtbox.AppendText(text);
+            }
+        }
+    }
+}
